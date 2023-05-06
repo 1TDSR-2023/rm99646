d2af322629a7b724364a40b339a0e926e4940a09
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2023-05-05 22:14:05 -0300

    Array/ declaration/funtions

--- js/script.js
+++ js/script.js
@@ -7,3 +7,39 @@ if(nome1 != ""){
 }
 
 console.log(nome1);
+
+//Declarar um Array
+let frutas = ["banana","maçã","pêssego","laranja","limão","melancia","kiwi"]
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[0]);
+console.log(frutas[6]);
+
+//Inserir um item ao final do array com o método push(nomeDoItem)
+frutas.push("manga");
+console.log(frutas);
+
+//Inserir um item ao final do array com o método push(nomeDoItem)
+frutas.push("pitaya");
+console.log(frutas);
+
+//Remover um item no final do array com o método shift();
+frutas.shift();
+console.log(frutas);
+
+//Remover um item no ínicio do array com o método pop();
+frutas.pop();
+console.log(frutas);
+
+//Localizar um item no array utilizando o método indexOf(nomeDoItem);
+//Obs: Este método retorna o índice do item encontrado.
+//let indice = frutas.indexOf("laranja");
+//console.log(`O índice encontrado foi : $(índice)`);
+//console.log(`O item buscado foi : ${frutas[indice]}`);
+
+//let indice = frutas.indexOf("melancia");
+//console.log(`O item encontrado no ímdice antes da remoção foi : ${ frutas[indice]}`);
+//frutas.splice(indice,1);
+//console.log(frutas);
+//console.log(`O item encontrado do índice depois da correção foi : ${ frutas[indice]}`)
+
